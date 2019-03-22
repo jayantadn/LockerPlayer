bd67ac5738d8bd68a4d84a0a28ad1738732f4668
Jayanta Debnath <jayanta.dn@gmail.com>
2019-03-22 20:50:51 +0530

    rmdir recursive becoming cumbersome in python

--- LockerPlayer.py
+++ LockerPlayer.py
@@ -1,12 +1,17 @@
+# imports
 import sys
 import os
-from pathlib import Path
 
+# constants
 CURDIR = os.path.dirname( sys.argv[0] )
+TMPDIR = os.path.join( CURDIR, "tmp" )
 CONFIGFILE = os.path.join( CURDIR, "config.txt" )
 CONFIG = dict()
 
-def main():
+def cleanup():
+    os.rmdir( TMPDIR )
+
+def getConfig():
     # check if config file exist, else exit
     if not os.path.exists( CONFIGFILE ):
         print( "ERROR: config file does not exist" )
@@ -15,7 +20,7 @@ def main():
     # open the config file and read all parameters
     fo = open( CONFIGFILE, "r" )
     while(True):
-        line = fo.readline()
+        line = fo.readline() # this will include the newline character which needs to be removed later
         if not line: break
         key = line.split("=")[0]
         val = line.split("=")[1]
@@ -30,6 +35,10 @@ def main():
         print( "ERROR: Configured movie player does not exist" )
         exit
     if not os.path.isfile(CONFIG["SPLITTER"][:-1]):
-        print( "WARNING: Configured movie splitter does not exist" )
+        print( "WARNING: Configured movie splitter does not exist" )    
+
+def main():
+    getConfig()
+    cleanup()
     
 main()
