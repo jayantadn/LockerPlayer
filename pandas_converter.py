975f8e8c2f2682e6b0526b83c76b5ee811974126
DJ1411 <jayanta.dn@gmail.com>
2022-11-15 22:28:07 +0530

    pandas converter complete

--- pandas_converter.py
+++ pandas_converter.py
@@ -2,6 +2,9 @@ import pandas as pd
 import json
 from datetime import datetime
 
+from actordb import ACTORDB
+actordb = ACTORDB()
+
 arrMovies = []
 fo = open('moviedb.json', 'r')
 contents = fo.read()
@@ -9,16 +12,7 @@ if not len(contents) == 0:
     arrMovies = json.loads(contents)
     fo.close()
 
-header = {
-    'timestamp': [datetime.now().strftime("%Y-%m-%d_%H:%M:%S")],
-    'rel_path': ["header"],
-    'rating': [0],
-    'playcount': [0],
-    'actor': ["header"],
-    'category': ["header"]
-}
-
-df = pd.DataFrame(header)
+df = pd.DataFrame()
 for movie in arrMovies:
     dftemp = pd.DataFrame({
         'timestamp': [movie['timestamp']],
@@ -26,12 +20,10 @@ for movie in arrMovies:
         'rating': [movie['rating']],
         'playcount': [movie['playcount']],
         'actor': [movie['actor']],
-        'category': [movie['category']]
+        'category': [movie['category']],
+        'actor_rating': [actordb.getRating(movie['actor'])]
     })
     df = pd.concat([df, dftemp], ignore_index=True)
 
-select = df['actor'] == "Kagney Linn Karter"
-print(df[select])
 
 df.to_excel('moviedb.xlsx')
-df.to_json('moviedb_pandas.json')
